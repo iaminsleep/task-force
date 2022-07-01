fba9423ae2a73567e20e4792c4261a7fd7c8bd4a
iaminsleep <xenydok1292@gmail.com>
2022-07-01 22:41:25 +0300

    fix

--- resources/views/account/partials/settings-password.blade.php
+++ resources/views/account/partials/settings-password.blade.php
@@ -1,10 +1,10 @@
 <div class="account__input">
     <label for="211">Новый пароль</label>
-    <input class="input textarea" type="password" id="211" name="password">
+    <input class="input textarea" type="password" id="211" name="password" autocomplete="off">
 </div>
 <div class="account__input">
     <label for="212">Повтор пароля</label>
-    <input class="input textarea" type="password" id="212" name="password_confirmation">
+    <input class="input textarea" type="password" id="212" name="password_confirmation" autocomplete="off">
     @error('password')
         <span class="upload-error">{{ $errors->first('password') }}</span>
     @enderror
